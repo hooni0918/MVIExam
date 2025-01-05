aca6baa7c78dfd6a84bcd2817c25d79ea6e4c345
hooni <jhlee909090@gmail.com>
2025-01-05 16:32:50 +0900

    Add new Xcode project configuration settings and build phases
    The commit introduces new Xcode project configurations, build phases, and settings for a project. It includes file references, target configurations, and build configuration lists.

---
+++ MVIExam/MVIExam/MVIExamApp.swift
@@ -0,0 +1,17 @@
+//
+//  MVIExamApp.swift
+//  MVIExam
+//
+//  Created by 이지훈 on 1/5/25.
+//
+
+import SwiftUI
+
+@main
+struct MVIExamApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
